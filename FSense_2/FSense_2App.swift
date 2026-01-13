887c6bdf7829acfe7115b6bec4dba418ac186f3e
demoffsrmain <ustimdima2002@gmail.com>
2026-01-13 11:25:50 +0100

    Initial Commit

---
+++ FSense_2/FSense_2App.swift
@@ -0,0 +1,17 @@
+//
+//  FSense_2App.swift
+//  FSense_2
+//
+//  Created by Dmitry Demidov on 13.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct FSense_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
